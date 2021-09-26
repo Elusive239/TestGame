b7212069fcae8700e0dc076a1bd9f0a4ba7237db
Elusive239 <thomasmcguff2727@gmail.com>
2021-09-26 00:25:08 -0400

    Quick Commit Through Alias

--- TestGame/Assets/Scripts/CameraController.cs
+++ TestGame/Assets/Scripts/CameraController.cs
@@ -1,3 +1,39 @@
-public class CameraController{
-    
+public class CameraController : MonoBehavior{
+    public Transfrom self;
+    public float speed;
+    private Vector2 dimensions;
+
+    public void Awake(){
+        self = this.transfrom;
+        dimensions = new Vector2((float)Screen.width / 2.0f, (float)Screen.height / 2.0f);
+    }
+
+    public void Update(){
+        // Handle screen touches.
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            // Move the cube if the screen has the finger moving.
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                pos.x = (pos.x - dimensions.x) / dimensions.x;
+                pos.y = (pos.y - dimensions.y) / dimensions.y;
+            }
+
+            if (Input.touchCount >= 2){
+                touch = Input.GetTouch(1);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    
+                }
+            }
+        }
+    }
 }
