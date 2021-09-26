94e55f3e98fae93111689b599aab8aeb6a54da1d
Elusive239 <thomasmcguff2727@gmail.com>
2021-09-26 11:25:12 -0400

    Quick Commit Through Alias

--- TestGame/Assets/Scripts/CameraController.cs
+++ TestGame/Assets/Scripts/CameraController.cs
@@ -2,13 +2,24 @@ using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour{
-    public float speed;
+    public float speed, padding;
+    public Vector2 boundsMin, boundsMax;
     private Transform self;
-    private Vector2 dimensions;
+    private Vector2 centerOfScreen;
 
     public void Awake(){
         self = this.transform;
-        dimensions = new Vector2((float)Screen.width / 2.0f, (float)Screen.height / 2.0f);
+        centerOfScreen = new Vector2((float)Screen.width / 2.0f, (float)Screen.height / 2.0f);
+        Vector3 upperBounds = Camera.main.ScreenToWorldPoint(new Vector3((float)Screen.width,  (float)Screen.height));
+        Vector3 lowerBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0));
+        boundsMin = new Vector2(
+            lowerBounds.x - padding,
+            lowerBounds.y - padding
+        );
+        boundsMax = new Vector2(
+            upperBounds.x + padding,
+            upperBounds.y + padding
+        );
     }
 
     public void Update(){
@@ -16,13 +27,6 @@ public class CameraController : MonoBehaviour{
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            // Move the cube if the screen has the finger moving.
-            if (touch.phase == TouchPhase.Moved)
-            {
-                Vector2 pos = touch.position;
-                pos = Vector2.MoveTowards(self.position, pos, speed*Time.deltaTime);
-            }
-
             if (Input.touchCount >= 2){
                 Touch touch2 = Input.GetTouch(1);
 
@@ -35,6 +39,19 @@ public class CameraController : MonoBehaviour{
                 {
                     
                 }
+                return;
+            }
+            // Move if the screen has the finger moving.
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                pos = Vector2.MoveTowards(self.position, pos, speed*Time.deltaTime);
+                                
+                pos = new Vector2 (
+                    Mathf.Clamp(pos.x, boundsMin.x, boundsMax.x), 
+                    Mathf.Clamp(pos.y, boundsMin.y, boundsMax.y) 
+                );
+                self.position = pos;
             }
         }
     }
